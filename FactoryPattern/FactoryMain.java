47d03f47c8f1413d454a03550498bf52f7c27aa4
Shobhit Tripathi <61062688+ShobhitTripathi@users.noreply.github.com>
2021-11-24 01:53:29 +0530

    Add files via upload

---
+++ FactoryPattern/FactoryMain.java
@@ -0,0 +1,10 @@
+package FactoryPattern;
+
+public class FactoryMain {
+
+    public static void main(String[] args) {
+        OperatingSystemFactory osf = new OperatingSystemFactory();
+        OS obj = osf.getInstance("Open");
+        obj.spec();
+    }
+}
